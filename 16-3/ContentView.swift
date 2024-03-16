0f354b270a08d22c4a8281bd75fccc342d04e88e
sysbe <sysbr@outlook.com>
2024-03-16 15:00:34 +0300

    Initial Commit

---
+++ 16-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  16-3
+//
+//  Created by MUHAMMED SABIR on 16.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 16-3/_6_3App.swift
@@ -0,0 +1,17 @@
+//
+//  _6_3App.swift
+//  16-3
+//
+//  Created by MUHAMMED SABIR on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct _6_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
