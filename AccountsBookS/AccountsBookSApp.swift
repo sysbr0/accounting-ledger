bb99516c5778c1eb605ae93b3f000bb3e627f894
sysbr <sysbr@outlook.com>
2024-04-04 14:08:23 +0300

    Initial Commit

---
+++ AccountsBookS/AccountsBookSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountsBookSApp.swift
+//  AccountsBookS
+//
+//  Created by MUHAMMED SABIR on 4.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccountsBookSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
