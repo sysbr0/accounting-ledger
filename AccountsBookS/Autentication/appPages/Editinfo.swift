2ededd29bc799743b0c6ca333634477d7099acdd
sysbr <sysbr@outlook.com>
2024-04-05 16:32:09 +0300

    edit profile information such as email

---
+++ AccountsBookS/Autentication/appPages/Editinfo.swift
@@ -0,0 +1,18 @@
+//
+//  editInfo.swift
+//  AccountsBookS
+//
+//  Created by MUHAMMED SABIR on 5.04.2024.
+//
+
+import SwiftUI
+
+struct editInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    editInfo()
+}
