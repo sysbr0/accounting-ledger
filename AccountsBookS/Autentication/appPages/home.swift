305eac82b613d0d5a9b0cb026324bd939b6fba88
sysbr <sysbr@outlook.com>
2024-04-15 10:35:10 +0300

    add navbar to swap betwen the views

---
+++ AccountsBookS/Autentication/appPages/home.swift
@@ -0,0 +1,8 @@
+//
+//  home.swift
+//  AccountsBookS
+//
+//  Created by MUHAMMED SABIR on 15.04.2024.
+//
+
+import Foundation
---
+++ AccountsBookS/Autentication/appPages/secound.swift
@@ -0,0 +1,18 @@
+//
+//  secound.swift
+//  AccountsBookS
+//
+//  Created by MUHAMMED SABIR on 15.04.2024.
+//
+
+import SwiftUI
+
+struct secound: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    secound()
+}
