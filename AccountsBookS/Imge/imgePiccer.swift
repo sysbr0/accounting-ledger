60005dbc65a8b8d53b6b70f07aa3a5f65338579a
MOHAMMED ALSBR <112288155+sysbr0@users.noreply.github.com>
2024-05-20 01:05:06 +0300

    add user imge

---
+++ AccountsBookS/Imge/imgePiccer.swift
@@ -0,0 +1,18 @@
+//
+//  imgePiccer.swift
+//  AccountsBookS
+//
+//  Created by MUHAMMED SABIR on 19.05.2024.
+//
+
+import SwiftUI
+
+struct imgePiccer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    imgePiccer()
+}
